9b12bc6b6e58150e1b7b65a04916f9a26b9be692
forzzh <83986976@qq.com>
2017-01-10 13:39:43 +0800

    增加编码配置和api修改

--- shop-manage-web/src/main/java/cn/shop/manage/controller/api/ApiItemCatController.java
+++ shop-manage-web/src/main/java/cn/shop/manage/controller/api/ApiItemCatController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,13 +17,32 @@ public class ApiItemCatController {
 	@Autowired
 	private ItemCatService itemCatService;
 	
+//	private static final ObjectMapper MAPPER = new ObjectMapper();
+	
+//	@RequestMapping(method = RequestMethod.GET)
+//	public ResponseEntity<String> queryByItemId(String callblack) {
+//		try {
+//			ItemCatResult tree = itemCatService.queryAllToTree();
+//			
+//			String json = MAPPER.writeValueAsString(tree);
+//			if (StringUtils.isEmpty(callblack)) {
+//				return ResponseEntity.ok(json);
+//			} else {
+//				StringBuilder builder = new StringBuilder(callblack);
+//				builder.append("(").append(json).append(")");
+//				return ResponseEntity.ok(builder.toString());
+//			}
+//			
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+//	}
+//	
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<ItemCatResult> queryByItemId(@PathVariable("itemId") Long itemId) {
+	public ResponseEntity<ItemCatResult> queryByItemId() {
 		try {
-			ItemCatResult tree = itemCatService.queryAllToTree();;
-			if (tree == null) {
-				return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-			}
+			ItemCatResult tree = itemCatService.queryAllToTree();
 			return ResponseEntity.ok(tree);
 		} catch (Exception e) {
 			e.printStackTrace();
